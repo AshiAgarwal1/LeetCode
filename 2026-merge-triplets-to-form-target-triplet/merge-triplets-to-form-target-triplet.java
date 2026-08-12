66332dc63b666e77fc2c6713ca0bb95e86b629c3
AshiAgarwal1 <agarwalashi231@gmail.com>
2026-08-12 14:00:51 +0530

    Time: 8 ms (8.15%) | Memory: 168.9 MB (16.22%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
@@ -1,13 +1,29 @@
 class Solution {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
-    Set<Integer> set=new HashSet<>();
-    for(int[] t:triplets){
-        if(t[0]>target[0] || t[1]>target[1] || t[2]>target[2]) continue;//if at any idx val greater than target ignore
-        //for remaining triplets if their value matches add their idx 
-        for(int i=0;i<t.length;i++){
-            if(t[i]==target[i]) set.add(i);
+        boolean x = false;
+        boolean y = false;
+        boolean z = false;
+//we dont change x,y,z values in loop , they continue their value to next iteration
+        for (int[] t : triplets) {
+            // Can this triplet give us target[0]?
+            if (t[0] == target[0] &&
+                t[1] <= target[1] &&
+                t[2] <= target[2]) {
+                x = true;
+            }
+            // Can this triplet give us target[1]?
+            if (t[0] <= target[0] &&
+                t[1] == target[1] &&
+                t[2] <= target[2]) {
+                y = true;
+            }
+            // Can this triplet give us target[2]?
+            if (t[0] <= target[0] &&
+                t[1] <= target[1] &&
+                t[2] == target[2]) {
+                z = true;
+            }
         }
-    } 
-    return set.size()==3;   
+        return x && y && z;
     }
 }
