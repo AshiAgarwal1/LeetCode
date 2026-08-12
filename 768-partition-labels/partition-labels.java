244ea4666915279fcc25d55e46910e67aa7f4768
AshiAgarwal1 <agarwalashi231@gmail.com>
2026-08-12 20:12:23 +0530

    Time: 5 ms (80.38%) | Memory: 43.3 MB (89.91%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -1,14 +1,14 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        HashMap<Character,Integer> map=new HashMap<>();
+        int[] arr=new int[26];
         for(int i=0;i<s.length();i++){
             //store the last occurrence of each character
-            map.put(s.charAt(i),i);
+            arr[s.charAt(i)-'a']=i;
         }
         List<Integer> res=new ArrayList<>();
         int start=0, end=0;
         for(int i=0;i<s.length();i++){
-            end=Math.max(end,map.get(s.charAt(i)));
+            end=Math.max(end,arr[s.charAt(i)-'a']);
             if(i==end){
                 res.add(end-start+1);
                 start=end+1;
