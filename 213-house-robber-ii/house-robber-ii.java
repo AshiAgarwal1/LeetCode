a47fe4cf4d689361efa1fca5db3ee28490775b46
AshiAgarwal1 <agarwalashi231@gmail.com>
2026-08-14 22:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (78.56%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -15,9 +15,9 @@ class Solution {
         int n=nums.length;
         //if there is only one house 
         if(n==1) return nums[0];
-        //rob every house except first one 
-        int[] temp1=Arrays.copyOfRange(nums,0,n-1);//last indx is excluded
         //rob every house except last one 
+        int[] temp1=Arrays.copyOfRange(nums,0,n-1);//last indx is excluded
+        //rob every house except first one 
         int[] temp2=Arrays.copyOfRange(nums,1,n);
         return Math.max(helper(temp1),helper(temp2));//either first or last will be robbed as adjacent
     }
