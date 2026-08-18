415fac1955ea991cfc2d6544df79fb4466ce5623
AshiAgarwal1 <agarwalashi231@gmail.com>
2026-08-18 14:24:02 +0530

    Time: 6 ms (62.04%) | Memory: 42.9 MB (69.03%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int res=0;
+        for(int i=0;i<s.length();i++){
+            res+=count(s,i,i);//count odd length palindrome
+            res+=count(s,i,i+1);//count even length palindrome    
+        }
+        return res;}
+    public int count(String s,int l,int r){
+        int cnt=0;
+        while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+            cnt++;
+            l--;
+            r++;
+        }
+        return cnt;
+    }  
+}
