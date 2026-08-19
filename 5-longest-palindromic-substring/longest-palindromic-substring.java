b2f5b4008391007b0bdba3057b9d1929c175338e
AshiAgarwal1 <agarwalashi231@gmail.com>
2026-08-19 14:53:33 +0530

    Time: 11 ms (97.64%) | Memory: 44.6 MB (43.69%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,31 +1,40 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        int n=s.length();
-        int maxLen=0;
-        int start=0;
+    public int[] manachers(String s){
+        StringBuilder t=new StringBuilder("#");
+        for(char c:s.toCharArray()){
+            t.append(c).append("#");
+        }
+        int n=t.length();
+        int[] p=new int[n];
+        int l=0,r=0;
         for(int i=0;i<n;i++){
-            //odd length
-            int l=i,r=i;
-            while(l>=0 && r<n && s.charAt(l)==s.charAt(r)){
-                if(r-l+1>maxLen){
-                    maxLen=r-l+1;
-                    start=l;
-                }
-                l--;
-                r++;
+            //mirror=l+(r-i)
+            p[i]=(i<r)?Math.min(p[l+(r-i)],r-i):0;
+            //if next left and right equal expand
+            while(i+p[i]+1<n && i-p[i]-1>=0 && t.charAt(i+p[i]+1)==t.charAt(i-p[i]-1)){
+                p[i]++;
             }
-            //even length
-             l=i;
-             r=i+1;
-            while(l>=0 && r<n && s.charAt(l)==s.charAt(r)){
-                if(r-l+1>maxLen){
-                    maxLen=r-l+1;
-                    start=l;
-                }
-                l--;
-                r++;
+            //update if found bigger palindrome
+            if(i+p[i]>r){
+                l=i-p[i];
+                r=i+p[i];
             }
         }
-        return s.substring(start,start+maxLen);
+        return p;
+    }
+    public String longestPalindrome(String s) {
+        //Manachers algo
+        int[] p=manachers(s);
+        int centerIdx=0, resLen=0;
+        for(int i=0;i<p.length;i++){
+            if(p[i]>resLen){
+                resLen=p[i];
+                centerIdx=i;
+            }
+        }
+        //left bound=center-radius 
+        //orig idx=left/2
+        int resIdx=(centerIdx-resLen)/2;
+        return s.substring(resIdx,resIdx+resLen);
     }
 }
