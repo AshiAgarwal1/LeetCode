0dd23ab84f6a7c172c39a5ea1c9c9fec0fe66a93
AshiAgarwal1 <agarwalashi231@gmail.com>
2026-08-25 21:58:05 +0530

    Time: 9 ms (24.63%) | Memory: 46.2 MB (47.63%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,18 +1,17 @@
 class Solution {
-    private Boolean[] dp;
-    int n;
-    private boolean solve(int idx,String s,List<String> wordDict){
-        if(idx==n) return true;
-        if(dp[idx] != null) return dp[idx];
-        for(int l=1;l<=n-idx;l++){
-            String split=s.substring(idx,idx+l);
-            if(wordDict.contains(split) && solve(idx+l,s,wordDict)) return dp[idx]=true;
-        }
-        return dp[idx]=false;
-    }
     public boolean wordBreak(String s, List<String> wordDict) {
-        n=s.length();
-        dp=new Boolean[n];
-        return solve(0,s,wordDict);
+        int n=s.length();
+        boolean[] dp=new boolean[n+1];
+        dp[n]=true;
+        for(int idx=n-1;idx>=0;idx--){
+            for(int l=1;l<=n-idx;l++){
+                String split=s.substring(idx,idx+l);
+                if(wordDict.contains(split) && dp[idx+l]) {
+                    dp[idx]=true;
+                    break;
+            }
+        }
+        }
+        return dp[0];
     }
 }
