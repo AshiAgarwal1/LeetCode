eb7b7d2cda1888d92575b6c4841699c966c84f5e
AshiAgarwal1 <agarwalashi231@gmail.com>
2026-08-26 18:55:51 +0530

    Time: 9 ms (24.58%) | Memory: 46.2 MB (34.55%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -3,10 +3,11 @@ class Solution {
         int n=s.length();
         boolean[] dp=new boolean[n+1];
         dp[n]=true;
+        Set<String> set=new HashSet<>(wordDict);
         for(int idx=n-1;idx>=0;idx--){
             for(int l=1;l<=n-idx;l++){
                 String split=s.substring(idx,idx+l);
-                if(wordDict.contains(split) && dp[idx+l]) {
+                if(set.contains(split) && dp[idx+l]) {
                     dp[idx]=true;
                     break;
             }
