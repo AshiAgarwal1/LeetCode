9ce59cbe80df7fb8ebb3c99e8c8773188dc25f5a
AshiAgarwal1 <agarwalashi231@gmail.com>
2026-09-06 22:15:43 +0530

    Time: 6 ms (85.06%) | Memory: 46.7 MB (26.39%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -0,0 +1,51 @@
+class Pair{
+    int first,second;
+    Pair(int first,int second){
+        this.first=first;
+        this.second=second;
+    }
+}
+class Tuple{
+    int first,second,third;
+    Tuple(int first,int second,int third){
+        this.first=first;
+        this.second=second;
+        this.third=third;
+    }
+}
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        ArrayList<ArrayList<Pair>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] f:flights){
+            adj.get(f[0]).add(new Pair(f[1],f[2]));
+        }
+        Queue<Tuple> q=new LinkedList<>();
+        q.add(new Tuple(0,src,0));
+        int[] dist=new int[n];
+        for(int i=0;i<n;i++){
+            dist[i]=(int)(1e9);
+        }
+        dist[src]=0;
+        while(!q.isEmpty()){
+            Tuple curr=q.peek();
+            q.remove();
+            int stops=curr.first;
+            int node=curr.second;
+            int cost=curr.third;
+            if(stops>k) continue;
+            for(Pair x:adj.get(node)){
+                int adjNode=x.first;
+                int edWt=x.second;
+                if(cost+edWt<dist[adjNode] && stops<=k){
+                    dist[adjNode]=cost+edWt;
+                    q.add(new Tuple(stops+1,adjNode,cost+edWt));
+                }
+            }
+        }
+        if(dist[dst]==1e9) return -1;
+        return dist[dst];
+    }
+}
