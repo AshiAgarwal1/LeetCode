d5cc81fa503d2963b88b7249417768039c383913
AshiAgarwal1 <agarwalashi231@gmail.com>
2026-09-09 18:02:42 +0530

    Time: 1 ms (38.16%) | Memory: 46.7 MB (78.73%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,34 +1,26 @@
-class Pair{
-    int first,second;
-    Pair(int first,int second){
-        this.first=first;
-        this.second=second;
-    }
-}
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int n=image.length;
-        int m=image[0].length;
-        Queue<Pair> q=new LinkedList<>();
+    int n;
+    int m;
+    public void dfs(int[][] image,int row,int col,int color,int original_color){
+        image[row][col]=color;
+        
         int[] dr={-1,0,1,0};
         int[] dc={0,1,0,-1};
-        int original_color=image[sr][sc];
-        if(original_color==color) return image;
-        image[sr][sc]=color;
-        q.add(new Pair(sr,sc));
-        while(!q.isEmpty()){
-            Pair curr=q.remove();
-            int row=curr.first;
-            int col=curr.second;
             for(int i=0;i<4;i++){
                 int nrow=row+dr[i];
                 int ncol=col+dc[i];
                 if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==original_color){
-                    image[nrow][ncol]=color;
-                    q.add(new Pair(nrow,ncol));
-                }
-            }
-        }
+                   dfs(image,nrow,ncol,color,original_color);
+                   }
+                   }
+                 }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        n=image.length;
+        m=image[0].length;
+        int original_color=image[sr][sc];
+        if(original_color==color) return image;
+        dfs(image,sr,sc,color,original_color);
+         
     return image;
     }
 }
