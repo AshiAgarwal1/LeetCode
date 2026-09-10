b7fc126188b2bd8045d7b418da83b103ac3d0cc7
AshiAgarwal1 <agarwalashi231@gmail.com>
2026-09-10 22:14:51 +0530

    Time: 11 ms (16.33%) | Memory: 53.5 MB (88.50%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,52 +1,47 @@
+class Pair{
+    int first,second;
+    Pair(int first,int second){
+        this.first=first;
+        this.second=second;
+    }
+}
 class Solution {
-    int m;
-    int n;
-    int[] delRow={-1,0,0,1};
-    int[] delCol={0,-1,1,0};
-    private void dfs(int row,int col,int[][] grid,int[][] vis){
-        vis[row][col]=1;
-        //check neighbours
-        for(int i=0;i<4;i++){
-            int nrow=row+delRow[i];
-            int ncol=col+delCol[i];
-            if(isValid(nrow,ncol) && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                dfs(nrow,ncol,grid,vis);
-            }
-        }
-
-    }
-    private boolean isValid(int r,int c){
-        return r>=0 && c>=0 && r<m && c<n;
-    }
     public int numEnclaves(int[][] grid) {
-        m=grid.length;
-        n=grid[0].length;
+        Queue<Pair> q=new LinkedList<>();
+        int m=grid.length;
+        int n=grid[0].length;
         int[][] vis=new int[m][n];
-        //traverse first and last row
-        for(int j=0;j<n;j++){
-            //first row
-            if(vis[0][j]==0 && grid[0][j]==1)
-            dfs(0,j,grid,vis);
-            //last row
-            if(vis[m-1][j]==0 && grid[m-1][j]==1)
-            dfs(m-1,j,grid,vis);
-        }
-        //traverse first col and last col
+        
         for(int i=0;i<m;i++){
-            //first col
-            if(vis[i][0]==0 && grid[i][0]==1)
-            dfs(i,0,grid,vis);
-            //last col
-            if(vis[i][n-1]==0 && grid[i][n-1]==1)
-            dfs(i,n-1,grid,vis);
-        }
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0 || i==m-1 || j==n-1){
+                    if(grid[i][j]==1){
+                    q.add(new Pair(i,j));
+                    vis[i][j]=1;}
+                }
+            }
+            }
+            while(!q.isEmpty()){
+                Pair curr=q.remove();
+                int row=curr.first;
+                int col=curr.second;
+                int[] delRow={-1,0,0,1};
+                int[] delCol={0,-1,1,0};
+                for(int i=0;i<4;i++){
+                    int nrow=row+delRow[i];
+                    int ncol=col+delCol[i];
+                    if(nrow>=0 && ncol>=0 && nrow<m && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                        q.add(new Pair(nrow,ncol));
+                        vis[nrow][ncol]=1;
+                    }
+                }
+            }   
         int cnt=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(vis[i][j]==0 && grid[i][j]==1) cnt++;
             }
         }
-    
     return cnt;
     }
 }
