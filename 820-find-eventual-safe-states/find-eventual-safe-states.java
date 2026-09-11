ef9c3fb2c8ac418f592139aea759b6e01fd90afe
AshiAgarwal1 <agarwalashi231@gmail.com>
2026-09-11 20:30:07 +0530

    Time: 26 ms (36.93%) | Memory: 60.6 MB (78.96%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,35 +1,31 @@
 class Solution {
-    private boolean dfs(int i,int[][] graph,int[] vis,int[] pathVis,int[] check){
-        vis[i]=1;
-        pathVis[i]=1;
-        check[i]=0;
-        //traverse for adjacent nodes
-        for(int it:graph[i]){
-            //if the node is not visited
-            if(vis[it]==0){
-                if(dfs(it,graph,vis,pathVis,check)==true) return true;//if cycle
-            }
-            //if node is visited but on the same path to be a cycle
-            else if(pathVis[it]==1) return true;
-        }
-        check[i]=1;//safe node
-        pathVis[i]=0;
-        return false;//no cycle
-    }
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int n=graph.length;
-        int[] vis=new int[n];
-        int[] pathVis=new int[n];
-        int[] check=new int[n];
+        ArrayList<ArrayList<Integer>> adjRev=new ArrayList<>();
         for(int i=0;i<n;i++){
-            if(vis[i]==0){
-                dfs(i,graph,vis,pathVis,check);
+            adjRev.add(new ArrayList<>());
+        }
+        int[] indegree=new int[n];
+        for(int i=0;i<n;i++){
+            for(int it:graph[i]){
+                adjRev.get(it).add(i);
+                indegree[i]++;
             }
         }
+        Queue<Integer> q=new LinkedList<>();
         List<Integer> safeNodes=new ArrayList<>();
         for(int i=0;i<n;i++){
-            if(check[i]==1) safeNodes.add(i);
+            if(indegree[i]==0) q.add(i);
         }
+        while(!q.isEmpty()){
+            int node=q.remove();
+            safeNodes.add(node);
+            for(int it:adjRev.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0) q.add(it);
+            }
+        }
+        Collections.sort(safeNodes);
         return safeNodes;
     }
 }
