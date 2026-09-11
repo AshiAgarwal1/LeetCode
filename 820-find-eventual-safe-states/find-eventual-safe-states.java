c766e4a6d73ad90e24884ce09207231191ce8978
AshiAgarwal1 <agarwalashi231@gmail.com>
2026-09-11 19:12:31 +0530

    Time: 4 ms (99.62%) | Memory: 65.5 MB (8.98%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -0,0 +1,42 @@
+class Solution {
+    private boolean dfs(int i,int[][] graph,int[] vis,int[] pathVis,int[] check){
+        vis[i]=1;
+        pathVis[i]=1;
+        check[i]=0;
+        //traverse for adjacent nodes
+        for(int it:graph[i]){
+            //if the node is not visited
+            if(vis[it]==0){
+                if(dfs(it,graph,vis,pathVis,check)==true) return true;//if cycle
+            }
+            //if node is visited but on the same path to be a cycle
+            else if(pathVis[it]==1) return true;
+        }
+        check[i]=1;//safe node
+        pathVis[i]=0;
+        return false;//no cycle
+    }
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int n=graph.length;
+        /*ArrayList<ArrayList<Integer>> adj= new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<n;i++){
+            adj.get(i).add()
+        }*/
+        int[] vis=new int[n];
+        int[] pathVis=new int[n];
+        int[] check=new int[n];
+        for(int i=0;i<n;i++){
+            if(vis[i]==0){
+                dfs(i,graph,vis,pathVis,check);
+            }
+        }
+        List<Integer> safeNodes=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(check[i]==1) safeNodes.add(i);
+        }
+        return safeNodes;
+    }
+}
