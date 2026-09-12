83176837088b64f051e42b33d25b31cec8a6225a
AshiAgarwal1 <agarwalashi231@gmail.com>
2026-09-12 22:40:58 +0530

    Time: 31 ms (18.72%) | Memory: 47.2 MB (11.70%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,34 +1,48 @@
+class Pair{
+    int dist,node;
+    Pair(int dist,int node){
+        this.dist=dist;
+        this.node=node;
+    }
+}
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int[][] dist=new int[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                dist[i][j]=Integer.MAX_VALUE;
-            }
-        }
+       //adjacency list
+       List<List<Pair>> adj=new ArrayList<>();
+       for(int i=0;i<n;i++){adj.add(new ArrayList<>());}
         for(int[] i:edges){
             int u=i[0];
             int v=i[1];
             int wt=i[2];
-            dist[u][v]=wt;
-            dist[v][u]=wt;
-        }
-        for(int i=0;i<n;i++) dist[i][i]=0;
-        //floyd warshalls algo
-        for(int k=0;k<n;k++){
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    if(dist[i][k]==Integer.MAX_VALUE || dist[k][j]==Integer.MAX_VALUE) continue;
-                    dist[i][j]=Math.min(dist[i][j],dist[i][k]+dist[k][j]);
-                }
-            }
+            adj.get(u).add(new Pair(wt,v));
+            adj.get(v).add(new Pair(wt,u));
         }
         int cntMax=n;
         int cityNo=-1;
+        //run dijkstra from every city
         for(int city=0;city<n;city++){
+            int[] dist=new int[n];
+            Arrays.fill(dist,Integer.MAX_VALUE);
+            PriorityQueue<Pair> pq=new PriorityQueue<>((x,y)->x.dist-y.dist);
+            dist[city]=0;
+            pq.add(new Pair(0,city));
+            while(!pq.isEmpty()){
+                Pair curr=pq.remove();
+                int d=curr.dist;
+                int node=curr.node;
+                if(d>dist[node]) continue;
+                for(Pair it:adj.get(node)){
+                    int wt=it.dist;
+                    int adjNode=it.node;
+                    if(d+wt<dist[adjNode]){
+                        dist[adjNode]=d+wt;
+                        pq.add(new Pair(d+wt,adjNode));
+                    }
+                }
+            }
             int cnt=0;
             for(int adjCity=0;adjCity<n;adjCity++){
-                if(dist[city][adjCity]<=distanceThreshold) cnt++;
+                if(dist[adjCity]<=distanceThreshold) cnt++;
             }
             if(cnt<=cntMax){
                 cntMax=cnt;
